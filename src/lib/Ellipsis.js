51ef962392f91105747cb608e9b16f9c4436c15f
Jelle <jelle.keulemans@gmail.com>
2021-10-24 14:05:43 +0200

    vector layer without duplicate protection and timer

--- src/lib/Ellipsis.js
+++ src/lib/Ellipsis.js
@@ -3,7 +3,9 @@ const Ellipsis = {
         return new EllipsisRasterLayer(blockId, captureId, visualizationId, maxZoom, token);
     },
 
-    VectorLayer: () => {
-        return new EllipsisVectorLayer();
+    VectorLayer: (blockId, layerId, selectFeature, token, 
+        styleId, filter, centerPoints = false, maxZoom = 21, pageSize = 25, maxMbPerTile = 16, 
+        maxTilesInCache = 500, maxFeaturesPerTile = 200, radius = 15, lineWidth = 5) => {
+        return new EllipsisVectorLayer(blockId, layerId, selectFeature, token, styleId, filter, centerPoints, maxZoom, pageSize, maxMbPerTile, maxTilesInCache, maxFeaturesPerTile, radius, lineWidth);
     }
 }
--- src/test/script.js
+++ src/test/script.js
@@ -1,6 +1,5 @@
 function init() {
     let map = L.map('mapid').setView([51.505, -0.09], 13);
-    console.log(map);
     const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -10,18 +9,20 @@ function init() {
         accessToken: 'pk.eyJ1IjoiZHV0Y2hqZWxseSIsImEiOiJja3Y0MXhsajc0ZHE3Mm5zNzhucTJiZXhmIn0.7UQPvzDakKqhRAUrUEQmxg'
     });
     
-    console.log(tileLayer);
     tileLayer.addTo(map);
 
     // Raster layer
-    Ellipsis.RasterLayer(
-        '01104b4f-85a7-482c-9ada-11dbce171982',
-        0,
-        '01f63a0d-3f92-42d3-925d-b3bfaf6dd6a1',
-        13
-    ).addTo(map)
-
-    Ellipsis.VectorLayer().addTo(map);
+    // Ellipsis.RasterLayer(
+    //     '01104b4f-85a7-482c-9ada-11dbce171982',
+    //     0,
+    //     '01f63a0d-3f92-42d3-925d-b3bfaf6dd6a1',
+    //     13
+    // ).addTo(map)
+    
+    Ellipsis.VectorLayer(
+        '1a24a1ee-7f39-4d21-b149-88df5a3b633a',
+        '45c47c8a-035e-429a-9ace-2dff1956e8d9'
+    ).addTo(map);
 
     // Vector layer
     // Ellipsis.VectorLayer(
